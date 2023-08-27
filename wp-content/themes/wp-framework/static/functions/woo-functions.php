296865e7bc01e0dee222027436aba77de2b08811
ajasra <das.ajasra@gmail.com>
2023-08-27 19:29:16 +0530

    wip: product description section on the right hand side

--- wp-content/themes/wp-framework/static/functions/woo-functions.php
+++ wp-content/themes/wp-framework/static/functions/woo-functions.php
@@ -13,3 +13,4 @@
 
 require_once(get_template_directory().'/functions/woo/woo-loop.php');
 require_once(get_template_directory().'/functions/woo/woo-shop.php');
+require_once(get_template_directory().'/functions/woo/woo-single.php');
