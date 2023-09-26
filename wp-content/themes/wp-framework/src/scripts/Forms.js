6ea99d1f8094c7d9f9d805a3b66b2894a78943eb
ajasra <das.ajasra@gmail.com>
2023-09-26 17:08:01 +0530

    fixed: the archive page views with wishlist buttons and select2 dropdowns

---
+++ wp-content/themes/wp-framework/src/scripts/Forms.js
@@ -0,0 +1,47 @@
+/* all forms js methods are here */
+
+import 'select2';
+
+export default class Forms {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.selectDropdown();
+    }
+
+    // select2 dropdowns
+    selectDropdown() {
+        // generic
+        $('select.form-control').each((i, elem) => {
+            let placeholder = $(elem).data('placeholder');
+            // add the placeholder for the dropdown as the first option of the select if cf7 form
+            if ($(elem).hasClass('wpcf7-select')) {
+                placeholder = $(elem).find('option:first-child').html();
+                // remove the first elem 
+                $(elem).find('option:first-child').html('');
+            }
+
+            $(elem).select2({
+                width: '100%',
+                placeholder: placeholder,
+                dropdownParent: $(elem).closest('.form-control-wrap'),
+                minimumResultsForSearch: -1
+            });
+        });
+
+
+        // woo archive page dropdown
+        let orderform = $('form.woocommerce-ordering')
+        orderform.find('select').select2({
+            width: '100%',
+            dropdownParent: $('form.woocommerce-ordering'),
+            minimumResultsForSearch: -1
+        });
+
+        orderform.find('select').on('change', e => {
+            orderform.trigger('submit');
+        });
+    }
+}
--- wp-content/themes/wp-framework/static/woocommerce/archive-product.php
+++ wp-content/themes/wp-framework/static/woocommerce/archive-product.php
@@ -95,7 +95,9 @@ echo "<div class=''>";
 					 * @hooked woocommerce_result_count - 20
 					 * @hooked woocommerce_catalog_ordering - 30
 					 */
-					do_action( 'woocommerce_before_shop_loop' );
+					echo "<div class='section-block woocommerce-before-loop'>";
+						do_action( 'woocommerce_before_shop_loop' );
+					echo "</div>";
 
 					woocommerce_product_loop_start();
 
