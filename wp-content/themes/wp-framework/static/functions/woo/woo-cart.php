da3fac58c5abfa041fe84897d2e2e3faf5415746
ajasra <das.ajasra@gmail.com>
2023-10-08 07:42:25 -0500

    added: the markupd and styles for the checkout and order received page

--- wp-content/themes/wp-framework/static/functions/woo/woo-cart.php
+++ wp-content/themes/wp-framework/static/functions/woo/woo-cart.php
@@ -23,3 +23,14 @@ function shady_custom_cart_thumbnail_size_filter($thumbnail, $cart_item, $cart_i
 
     return $modified_thumbnail;
 }
+
+
+
+/* overrdie the checkout page */
+
+// remove the billing company
+add_filter('woocommerce_checkout_fields', 'shady_remove_billing_fields');
+function shady_remove_billing_fields($fields) {
+    unset($fields['billing']['billing_company']);
+    return $fields;
+}
--- wp-content/themes/wp-framework/static/templates/page-cart-checkout.php
+++ wp-content/themes/wp-framework/static/templates/page-cart-checkout.php
@@ -5,19 +5,7 @@
 get_header();
 ?>
 
-<div class="single-page single-cart-checkout <?php echo is_cart() ? 'cart' : '';?>">
-    <?php if (!is_cart()) : ?>
-    <section class="section-block section-hero">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="section-title mb-0 color-grey"><?php the_title();?></h1>
-                </div>
-            </div>
-        </div>
-    </section>
-    <?php endif; ?>
-
+<div class="single-page single-cart-checkout <?php echo is_cart() ? 'cart' : 'checkout';?>">
     <section class="section-block section-content">
         <div class="container">
             <div class="row">
