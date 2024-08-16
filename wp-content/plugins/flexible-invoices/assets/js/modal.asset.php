5e5b3b12356ffc80b1c03b001119b733a814edd7
Ajasra Das <das.ajasra@gmail.com>
2024-08-17 04:53:12 +0530

    chore: removes unwanted invoice plugin

--- wp-content/plugins/flexible-invoices/assets/js/modal.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9c9a0dd2fc5ebf577548');
--- wp-content/plugins/flexible-invoices/vendor_prefixed/psr/log/Psr/Log/InvalidArgumentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace WPDeskFIVendor\Psr\Log;
-
-class InvalidArgumentException extends \InvalidArgumentException
-{
-}
