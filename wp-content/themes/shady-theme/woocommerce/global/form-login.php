b00d7da7ed4202641f3efa430792530699a0314d
Ajasra Das <das.ajasra@gmail.com>
2024-09-27 18:57:11 +0530

    fix: installs new login plugin and fixes the login bug

--- wp-content/themes/shady-theme/woocommerce/global/form-login.php
+++ wp-content/themes/shady-theme/woocommerce/global/form-login.php
@@ -10,9 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see         https://docs.woocommerce.com/document/template-structure/
+ * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     7.0.1
+ * @version     9.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -32,13 +32,12 @@ if ( is_user_logged_in() ) {
 
 	<div class="row">
 		<div class="form-group col-12 col-md-5 col-xl-4">
-			<label class="form-label" for="username"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-			<input type="text" class="input-text form-control" name="username" id="username" autocomplete="username" />
-		</div>
+			<label class="form-label" for="username"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span><span class="screen-reader-text"><?php esc_html_e( 'Required', 'woocommerce' ); ?></span></label>
+			<input type="text" class="input-text" name="username" id="username" autocomplete="username" required aria-required="true" />
 		<div class="w-100"></div>
 		<div class="form-group col-12 col-md-5 col-xl-4">
-			<label class="form-label" for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-			<input class="input-text form-control woocommerce-Input" type="password" name="password" id="password" autocomplete="current-password" />
+			<label class="form-label" for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required" aria-hidden="true">*</span><span class="screen-reader-text"><?php esc_html_e( 'Required', 'woocommerce' ); ?></span></label>
+			<input class="input-text woocommerce-Input" type="password" name="password" id="password" autocomplete="current-password" required aria-required="true" />
 		</div>
 	</div>
 	<div class="clear"></div>
